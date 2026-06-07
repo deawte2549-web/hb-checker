e5efdfa4632a6ccb2a516392a4ddca59751235e6
thanachot <deawte2549@gmail.com>
2026-06-08 00:30:51 +0700

    add WBC and Platelet

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,64 @@
-import './App.css'
 import { useState } from "react";
+import './App.css'
+
 
 function App() {
   const[gender, setGender] = useState('male')
   const [hb, setHb] = useState('')
-  const[result, setResult] =useState('')
-  const [color, setColor] = useState('white')
+  const[wbc, setWbc] = useState('')
+  const[platelet, setPlatelet] =useState('')
+  const[result, setResult] =useState([])
 
-function checkHb() {
-  const hbNum = Number(hb)   // แปลง string เป็นตัวเลข
-  let min = gender === 'male' ? 13 : 12
-  let max = gender === 'male' ? 17 : 16
+function checkAll() {
+  const hbNum = Number(hb)
+  const wbcNum = Number(wbc)
+  const plateletNum = Number(platelet)
+  const list = []
 
-  if (hbNum < min) {
-    setResult('ค่า Hb ต่ำกว่าปกติ')
-    setColor('red')
-  } else if (hbNum > max) {
-    setResult('ค่า Hb สูงกว่าปกติ')
-    setColor('red')
-  } else {
-    setResult('ค่า Hb ปกติ')
-    setColor('green')
+
+  //เช็ค hb
+  const hbMin = gender === 'male' ? 13 : 12
+  const hbMax = gender === 'male' ? 17 : 16
+  if (hbNum) {
+    if(hbNum < hbMin) list.push({ name: 'Hb', status: 'ต่ำกว่าปกติ', ok: false})
+    else if (hbNum > hbMax) list.push({ name: 'Hb', status: 'สูงกว่าปกติ', ok: false})
+    else list.push({ name: 'Hb', status: 'ปกติ', ok:true })
   }
-}
+
+  //เช็ค wbc
+  if (wbcNum) {
+    if (wbcNum < 4500) list.push({ name: 'wbc',status: 'ต่ำกว่าปกติ', ok:false})
+    else if(wbcNum > 11000) list.push({ name: 'wbc',status: 'สูงกว่าปกติ', ok: false})
+    else list.push({ name: 'wbc', status: 'ปกติ', ok:true })
+  }
+ if (plateletNum) {
+      if (plateletNum < 150000) list.push({ name: 'Platelet', status: 'ต่ำกว่าปกติ', ok: false })
+      else if (plateletNum > 400000) list.push({ name: 'Platelet', status: 'สูงกว่าปกติ', ok: false })
+      else list.push({ name: 'Platelet', status: 'ปกติ', ok: true })
+    }
+setResult(list)
+
+} 
 
   return(
     <div className='card'>
-      <h1>ตรวจสอบค่า Hemoglobin</h1>
+      <h1>ตรวจสอบค่าเลือด CBC</h1>
 
       <select onChange={(e) => setGender(e.target.value)}>
         <option value="male">ชาย</option>
         <option value="female">หญิง</option>
       </select>
 
-      <input
-        type="number"
-        placeholder="กรอกค่า Hb"
-        onChange={(e) => setHb(e.target.value)}
-        />
+      <input type="number" placeholder="Hb (g/dL)" onChange={(e) => setHb(e.target.value)} />
+      <input type="number" placeholder="WBC (เซลล์/ไมโครลิตร" onChange={(e) => setWbc(e.target.value)} />
+      <input type="number" placeholder="Platelet (เซลล์/ไมโครลิตร)" onChange={(e) => setPlatelet(e.target.value)} />
 
-      <button onClick={checkHb}>ตรวจสอบ</button>
-
-      <p style={{ color: color}}>{result}</p>
+      <button onClick={checkAll}>ตรวจสอบ</button>
+      {result.map((r, i) => (
+      <p key={i} className="result" style={{ color: r.ok ? 'green' : 'red'}}>
+        {r.name}: {r.status}
+      </p>
+      ))}
     </div>
   )
 }
